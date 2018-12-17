9fef522cb987842bf8a4dd21b17904670275e932
Hemant Jain <hmtj99@gmail.com>
2018-12-17 21:31:46 +0530

    Added Search Form to homepage

--- tweetify/search_app/views.py
+++ tweetify/search_app/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from search_app.forms import SearchForm
+
+from search_app.forms import SearchForm
 
 def index(request):
-    return render(request,"search_app/index.html",{})
+    form = SearchForm()
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            search = form.cleaned_data['search']
+            print("Topic is", search)
+    return render(request,"search_app/index.html",{'form':form})
