0ee5be60aec95f2767018d18db84a05dd59f744d
Hemant Jain <hmtj99@gmail.com>
2018-12-27 15:44:33 +0530

    Added Dynamic Changing Quotes To Homepage
    Reload Site to see diffferent quotes.

--- tweetify/search_app/views.py
+++ tweetify/search_app/views.py
@@ -3,6 +3,7 @@ from search_app.forms import SearchForm
 import tweepy
 from textblob import TextBlob
 import os
+import random
 
 from search_app.forms import SearchForm
 
@@ -30,8 +31,27 @@ def index(request):
                 blob = TextBlob(tweet.full_text)
                 sentiment = blob.sentiment.polarity
                 sentiment_list.append(sentiment)
-
+    
             result = zip(tweets,sentiment_list)    
             return render(request,"search_app/list.html",{'result':result})
 
-    return render(request,"search_app/index.html",{'form':form})
+    value = random.randint(0,30)
+    value2 = random.randint(0,30)
+
+    with open(os.path.dirname(os.path.realpath(__file__)) + '\quotes.txt','r') as f:
+        for  i,line in enumerate(f):
+            if i==value:
+                myline = line
+            if i==value2:
+                myline2 = line    
+                
+    line_comp = myline.split('-')
+    quote = line_comp[0]
+    source = line_comp[1]
+
+    line_comp2 = myline2.split('-')
+    quote2 = line_comp2[0]
+    source2 = line_comp2[1]
+         
+
+    return render(request,"search_app/index.html",{'form':form,'quote':quote,'source':source,'quote2':quote2,'source2':source2})
